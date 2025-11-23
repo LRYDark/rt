15f3a3f4fe84d0c3200e60316034fdfd45b0d077
LRYDark <reinert.joris@gmail.com>
2025-11-23 23:39:17 +0100

    1.6.0_beta4

--- setup.php
+++ setup.php
@@ -1,5 +1,5 @@
 <?php
-define('PLUGIN_RT_VERSION', '1.6.0_beta3'); // version du plugin
+define('PLUGIN_RT_VERSION', '1.6.0_beta4'); // version du plugin
 $_SESSION['PLUGIN_RT_VERSION'] = PLUGIN_RT_VERSION;
 
 // Minimal GLPI version,
